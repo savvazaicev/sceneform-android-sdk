73c642989d5b2bc45ff85428cba288b3ec9b70b8
Savva <oisurka@gmail.com>
2020-11-23 15:02:23 +0300

    1.15.4

--- sceneformux/ux/src/main/java/com/google/ar/sceneform/ux/BaseArFragment.java
+++ sceneformux/ux/src/main/java/com/google/ar/sceneform/ux/BaseArFragment.java
@@ -378,10 +378,6 @@ public abstract class BaseArFragment extends Fragment
 
         Session session = createSession();
 
-        if (this.onSessionInitializationListener != null) {
-          this.onSessionInitializationListener.onSessionInitialization(session);
-        }
-
         Config config = getSessionConfiguration(session);
         // Force the non-blocking mode for the session.
 
@@ -410,15 +406,14 @@ public abstract class BaseArFragment extends Fragment
     if (session == null) {
       session = new Session(requireActivity());
     }
+    if (this.onSessionInitializationListener != null) {
+      this.onSessionInitializationListener.onSessionInitialization(session);
+    }
     return session;
   }
 
-  /**
-   * Creates the ARCore Session with the with features defined in #getSessionFeatures. If this
-   * returns null, the Session will be created with the default features.
-   */
   
-  protected @Nullable Session createSessionWithFeatures()
+  Session createSessionWithFeatures()
       throws UnavailableSdkTooOldException, UnavailableDeviceNotCompatibleException,
           UnavailableArcoreNotInstalledException, UnavailableApkTooOldException {
     return new Session(requireActivity(), getSessionFeatures());
@@ -444,7 +439,6 @@ public abstract class BaseArFragment extends Fragment
   protected void setupSelectionRenderable(FootprintSelectionVisualizer selectionVisualizer) {
     ModelRenderable.builder()
         .setSource(getActivity(), R.raw.sceneform_footprint)
-        .setIsFilamentGltf(true)
         .build()
         .thenAccept(
             renderable -> {
